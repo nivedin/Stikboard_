ef34c5552dfc12c7c6873cb8af3348f5775451d1
Nivedin <nivedinp@gmail.com>
2020-12-07 15:30:37 +0530

    change in meta tag

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ const Index = () => {
             <meta name="description" content="
 Spend all day doing what you love the most.Meet like-minded people and learn.Expose your talents.Connect with people with similar talents around the world.Stikboard is here to stay." />
             <link rel="canonical" href={`${DOMAIN}`} />
-            <meta property="og:title" content={`Latest web blogs and news | ${APP_NAME}`} />
-            <meta name="og:description" content="Programming blogs,travell,photograph like a social media platform" />
+            <meta property="og:title" content={`Not your usual social network | ${APP_NAME}`} />
+            <meta name="og:description" content="Share your creation to the world,connect with similar talents and participate in exciting competitions." />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={`${DOMAIN}`} />
             <meta name="twitter:card" content={`${DOMAIN}/images/mainLogoMeta.jpg`}/>
@@ -24,7 +24,7 @@ Spend all day doing what you love the most.Meet like-minded people and learn.Exp
             <meta itemprop="description" content="Spend all day doing what you love the most.Meet like-minded people and learn.Expose your talents.Connect with people with similar talents around the world.Stikboard is here to stay." />
             <meta itemprop="image" content={`${DOMAIN}/images/mainLogoMeta.jpg`}/>
             <meta property="og:site_name" content="Stikboard"/>
-            <meta name="twitter:image:alt" ontent="Programming blogs,travell,photograph like a social media platform"/>
+            <meta name="twitter:image:alt" ontent="Share your creation to the world,connect with similar talents and participate in exciting competitions."/>
             <meta property="og:image" content={`${DOMAIN}/images/mainLogoMeta.jpg`} />
             <meta property="og:image:secure_url" content={`${DOMAIN}/images/mainLogoMeta.jpg`} />
             <meta property="og:image:type" content="image/png" />
